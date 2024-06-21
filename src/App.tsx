6d0c529f8defbfcc7ad9ca6a019b6e535da808bd
Sebastián Maciel <sebastianmaciel@outlook.com>
2024-06-21 01:26:06 -0300

    feat: Flowbite + react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { Card, Flowbite, Navbar, useThemeMode } from "flowbite-react";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>React Doctor</h1>;
+  const { setMode } = useThemeMode();
+
+  useEffect(() => {
+    setMode("dark");
+  }, [setMode]);
+
+  return (
+    <Flowbite>
+      <Navbar fluid rounded>
+        <Navbar.Brand href="https://flowbite-react.com">
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-gray-100">
+            OB Doc
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="#" active>
+            Config
+          </Navbar.Link>
+          <Navbar.Link href="#">List</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <section className="flex flex-col justify-center py-6">
+        <Card className="max-w-full">
+          <h5 className="text-2xl font-bold tracking-tight text-white">
+            NodeJS
+          </h5>
+          <p className="font-normal text-gray-400">
+            Let's configure the checking settings
+          </p>
+        </Card>
+      </section>
+    </Flowbite>
+  );
 }
 
 export default App;
